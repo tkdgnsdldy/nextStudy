aec7d32d0b840f101add641bb5e20dda03773038
LeeSanghun <you@example.com>
2020-10-20 07:06:30 +0900

    next study

---
+++ study/pages/groups/index.js
@@ -0,0 +1,9 @@
+import Nav from '../../components/nav';
+
+// React.Fragment
+export default () => <>
+    <Nav/>
+    <h1>Groups</h1>
+
+    <button className="btn btn-primary">Groups Button</button>
+</>
